7169cdf299804d84e6b29563b41669c86f412c42
Crowdbotics <engineering@crowdbotics.com>
2023-09-20 05:58:43 +0000

    Initial commit.

---
+++ backend/ghyt_dev_117233/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for ghyt_dev_117233 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "ghyt_dev_117233.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "ghyt-dev-117233",
+  url: "https://ghyt-dev-117233.botics.co",
+  api: "https://ghyt-dev-117233.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
